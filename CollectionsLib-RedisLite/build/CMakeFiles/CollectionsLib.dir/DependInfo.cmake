3010582ea507ff94a022cdd7a9c7aa5ccd5dcd43
vivek <vivel@1234>
2026-06-26 08:01:12 +0530

    adding properly running master library and include header guards on all programs

---
+++ CollectionsLib-RedisLite/build/CMakeFiles/CollectionsLib.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/projects/RedisLite/CollectionsLib-RedisLite/src/doublylinkedList.cpp" "CMakeFiles/CollectionsLib.dir/src/doublylinkedList.cpp.obj" "gcc" "CMakeFiles/CollectionsLib.dir/src/doublylinkedList.cpp.obj.d"
+  "D:/projects/RedisLite/CollectionsLib-RedisLite/src/dynamicArray.cpp" "CMakeFiles/CollectionsLib.dir/src/dynamicArray.cpp.obj" "gcc" "CMakeFiles/CollectionsLib.dir/src/dynamicArray.cpp.obj.d"
+  "D:/projects/RedisLite/CollectionsLib-RedisLite/src/hash.cpp" "CMakeFiles/CollectionsLib.dir/src/hash.cpp.obj" "gcc" "CMakeFiles/CollectionsLib.dir/src/hash.cpp.obj.d"
+  "D:/projects/RedisLite/CollectionsLib-RedisLite/src/hashmap.cpp" "CMakeFiles/CollectionsLib.dir/src/hashmap.cpp.obj" "gcc" "CMakeFiles/CollectionsLib.dir/src/hashmap.cpp.obj.d"
+  "D:/projects/RedisLite/CollectionsLib-RedisLite/src/linkedList.cpp" "CMakeFiles/CollectionsLib.dir/src/linkedList.cpp.obj" "gcc" "CMakeFiles/CollectionsLib.dir/src/linkedList.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CollectionsLib-RedisLite/build/CMakeFiles/CollectionsLib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CollectionsLib.dir/src/doublylinkedList.cpp.obj"
+  "CMakeFiles/CollectionsLib.dir/src/doublylinkedList.cpp.obj.d"
+  "CMakeFiles/CollectionsLib.dir/src/dynamicArray.cpp.obj"
+  "CMakeFiles/CollectionsLib.dir/src/dynamicArray.cpp.obj.d"
+  "CMakeFiles/CollectionsLib.dir/src/hash.cpp.obj"
+  "CMakeFiles/CollectionsLib.dir/src/hash.cpp.obj.d"
+  "CMakeFiles/CollectionsLib.dir/src/hashmap.cpp.obj"
+  "CMakeFiles/CollectionsLib.dir/src/hashmap.cpp.obj.d"
+  "CMakeFiles/CollectionsLib.dir/src/linkedList.cpp.obj"
+  "CMakeFiles/CollectionsLib.dir/src/linkedList.cpp.obj.d"
+  "libCollectionsLib.a"
+  "libCollectionsLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CollectionsLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
